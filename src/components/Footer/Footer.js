2bf21e1d63ddf94ee74029e54bd3dd88008da1f3
SHASHI KANT KUMAR <shashikant.py8@gmail.com>
2023-07-14 11:32:56 +0530

    input-box-improve

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
             </div>
 
             <div className="midFooter">
-                <h1>ECOMMERCE.</h1>
+                <h1>ECOMMERCE</h1>
                 <p>High Quality is our first priority</p>
 
                 <p>Copyrights 2022 &copy; shashikant-sde</p>
